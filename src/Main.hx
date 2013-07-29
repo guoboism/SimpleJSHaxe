5ace284aaba4c969fcc47969441f20302fdb3484
deadkidsong <deadkidsong@hotmail.com>
2013-07-30 01:23:41 +0800

    Add nore tests

--- src/Main.hx
+++ src/Main.hx
@@ -32,7 +32,7 @@ class Main extends Sprite
 	public function new() 
 	{
 		
-		stage.color = 0x996699;
+		//stage.color = 0x996699;
 		
 		super();	
 		addEventListener(Event.ADDED_TO_STAGE, added);
@@ -62,8 +62,9 @@ class Main extends Sprite
 		var ip:Interpreter = new Interpreter();
 		
 		var dict:Map<String, Dynamic> = new Map<String, Dynamic>();
-		dict["MovieClip"] = MovieClip;
-		dict["testAPI"] = testAPI;
+		dict.set("MovieClip", MovieClip); //bind type
+		dict.set("testAPI", testAPI);//bind function   [] is not allow with dynamic types
+		dict.set("gg", graphics); //bind object
 		ip.pushDict(dict);
 		
 		var startTime;
@@ -76,18 +77,26 @@ class Main extends Sprite
 		
 		trace((Lib.getTimer() - startTime));
 		*/
+		graphics.lineStyle(2, 0x996699);
+		graphics.drawCircle(100, 100, 100);
 		
 		ip.doString("var m = 5 + 6 * 8;");
 		ip.doString("var n = m + 1;");
 		ip.doString("var a = 6;\rwhile(a<1000){a=a+1;}");
 		ip.doString("function f(a){a=a+6;return a;}\rvar b = f(7);");
 		ip.doString("var test = testAPI(5);");
-		//ip.doString("gg.drawCircle(300, 300, 100);");
+		ip.doString("var mc = new MovieClip();");
+		ip.doString("var s = 3; s = testAPI(s);");
+		ip.doString("var s = 3; s = testAPI(s);");
+		ip.doString("gg.drawCircle(200, 200, 200);");
+		//ip.doString("gg.drawCircle(200, 200, 200);");
 		
 		trace("findVar m:" + ip.vm.findVar("m"));
 		trace("findVar n:" + ip.vm.findVar("n"));
 		trace("findVar a:" + ip.vm.findVar("a"));
 		trace("findVar b:" + ip.vm.findVar("b"));
+		trace("findVar mc:" + ip.vm.findVar("mc"));
+		trace("findVar s:" + ip.vm.findVar("s"));
 		
 		// Stage:
 		// stage.stageWidth x stage.stageHeight @ stage.dpiScale
